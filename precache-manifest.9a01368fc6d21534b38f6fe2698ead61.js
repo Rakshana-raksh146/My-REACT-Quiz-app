f10a5e7e0a04b712bbaa5d3202ea2eee282aabd1
Rakshana-raksh146 <rakshanadurairaj@gmail.com>
2020-01-27 22:27:00 +0530

    Updates

---
+++ precache-manifest.9a01368fc6d21534b38f6fe2698ead61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1b9de71afdf433cfc36db314ffcca1",
+    "url": "/My-REACT-Quiz-app.git/index.html"
+  },
+  {
+    "revision": "212612050e039d765c03",
+    "url": "/My-REACT-Quiz-app.git/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "5832d31d0ec814532351",
+    "url": "/My-REACT-Quiz-app.git/static/js/2.63be56d4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/My-REACT-Quiz-app.git/static/js/2.63be56d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "212612050e039d765c03",
+    "url": "/My-REACT-Quiz-app.git/static/js/main.120d4385.chunk.js"
+  },
+  {
+    "revision": "c9f374ad7d427cede610",
+    "url": "/My-REACT-Quiz-app.git/static/js/runtime-main.7bd7a1ad.js"
+  }
+]);
